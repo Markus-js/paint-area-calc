87235287ac1d3cf2495a2496f23040996b0145df
Markus-js <markusbackhausen@outlook.com>
2022-01-23 06:20:28 +0100

    fetch

--- src/context/states.js
+++ src/context/states.js
@@ -1,14 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
-  const [searchResult, setSearchResult] = useState([]);
+  const [items, setItems] = useState(null);
+
+  const SPACEID = process.env.REACT_APP_SPACEID;
+  const ACCESSTOKEN = process.env.REACT_APP_ACCESSTOKEN;
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const response = await fetch(
+        `https://cdn.contentful.com/spaces/${SPACEID}/environments/master/entries?access_token=${ACCESSTOKEN}`
+      );
+      const resJson = await response.json();
+      setItems(resJson.items);
+    };
+    fetchApi();
+  }, []);
 
   return (
     <AppContext.Provider
       value={{
-        searchResult,
+        items,
+        setItems,
       }}
     >
       {children}
