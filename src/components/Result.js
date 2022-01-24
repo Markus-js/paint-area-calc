6869c7d36b569d0aa4077c7e793212da031d0bf3
Markus-js <markusbackhausen@outlook.com>
2022-01-24 20:48:09 +0100

    result ui

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,8 +1,27 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AppContext } from "../context/states";
+import { BiLinkExternal } from "react-icons/bi";
 
 const Result = () => {
   const { result, selectedProduct, calculatedArea } = useContext(AppContext);
+  const [grunderArea, setGrunderArea] = useState(null);
+
+  useEffect(() => {
+    handleGrunder();
+  }, [calculatedArea]);
+
+  const handleGrunder = () => {
+    if (selectedProduct) {
+      // Calc ( l * h / sum of dækkeevne * lag )
+
+      setGrunderArea(
+        (
+          (result / selectedProduct.fields.dkkeevneGrunder) *
+          selectedProduct.fields.lagGrunder
+        ).toFixed(2)
+      );
+    }
+  };
 
   return (
     <div className="result-container container">
@@ -14,25 +33,65 @@ const Result = () => {
       </header>
       {selectedProduct && result !== 0 && (
         <>
-          <p>Dine mål:</p>
-          {result && <h2>Total areal: {result} ㎡</h2>}
-          <p>Mængde information:</p>
-          <h2>
-            Baseret på {selectedProduct.fields.lag} lag: {calculatedArea} liter
+          <h2 className="product-title">
+            {selectedProduct.fields.produktNavn}{" "}
+            {selectedProduct.fields.link && (
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={selectedProduct.fields.link}
+              >
+                <BiLinkExternal className="icon" />
+              </a>
+            )}
           </h2>
+          <p className="text-info">Dine mål:</p>
+          {result && <h3>Total areal: {result} ㎡.</h3>}
+          <p className="text-info">Mængde information:</p>
+          <h3>
+            Baseret på {selectedProduct.fields.lag} lag: {calculatedArea} liter.
+          </h3>
           {selectedProduct.fields.produktNavnGrunder &&
-          selectedProduct.fields.lagGrunder ? (
+          selectedProduct.fields.lagGrunder &&
+          selectedProduct.fields.dkkeevneGrunder ? (
             // Med Anbefalder Grunder
-            <p>
-              For det bedste resultat anbefaler vi at bruge{" "}
-              <b>{selectedProduct.fields.lagGrunder} lag</b> af{" "}
-              <b>{selectedProduct.fields.produktNavnGrunder}</b> før{" "}
-              <b>{selectedProduct.fields.lag} lag</b> af{" "}
-              <b>{selectedProduct.fields.produktNavn}</b>
-            </p>
+            <>
+              <p className="text-info">
+                For det bedste resultat anbefaler vi at bruge{" "}
+                <b>{selectedProduct.fields.lagGrunder} lag</b> af{" "}
+                <b>{selectedProduct.fields.produktNavnGrunder}</b> før{" "}
+                <b>{selectedProduct.fields.lag} lag</b> af{" "}
+                <b>{selectedProduct.fields.produktNavn}.</b>
+              </p>
+
+              {grunderArea !== null && (
+                <>
+                  <h2 className="product-title">
+                    {selectedProduct.fields.produktNavnGrunder}
+                    {selectedProduct.fields.linkGrunder && (
+                      <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href={selectedProduct.fields.linkGrunder}
+                      >
+                        <BiLinkExternal className="icon" />
+                      </a>
+                    )}
+                  </h2>
+
+                  <p className="text-info">Dine mål:</p>
+                  {result && <h3>Total areal: {result} ㎡.</h3>}
+                  <p className="text-info">Mængde information:</p>
+                  <h3>
+                    Baseret på {selectedProduct.fields.lagGrunder} lag:{" "}
+                    {grunderArea} liter.
+                  </h3>
+                </>
+              )}
+            </>
           ) : (
             // Uden Grunder
-            <p>
+            <p className="text-info">
               For det bedste resultat anbefaler vi at bruge{" "}
               <b>{selectedProduct.fields.lag} lag</b> af{" "}
               <b>{selectedProduct.fields.produktNavn}</b>
